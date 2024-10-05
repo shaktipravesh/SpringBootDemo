f28b290984a30f69792449223b485cb5363ea616
shaktipravesh <shaktipravesh@yahoo.co.in>
2024-10-05 15:04:25 +0530

    Initial version

---
+++ src/test/java/com/suhasini/learningdemo/LearningDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhasini.learningdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
